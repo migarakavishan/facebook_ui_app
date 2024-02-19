0e3c120900466aa80cebc57366b464b5144de4f7
migarakavishan <migarakavishan43@gmail.com>
2024-02-19 21:44:16 +0530

    commit 1 done

--- lib/screens/widgets/stories_list_view.dart
+++ lib/screens/widgets/stories_list_view.dart
@@ -2,14 +2,48 @@ import 'package:facebook_ui_app/models/model_story.dart';
 import 'package:flutter/material.dart';
 
 class StoriesListView extends StatelessWidget {
-  const StoriesListView({
+  StoriesListView({
     super.key,
-    required this.stories,
     required this.profilePic,
   });
 
-  final List<StoryModel> stories;
   final String profilePic;
+
+  final List<StoryModel> stories = [
+    StoryModel(
+        story: "story",
+        uid: "uid",
+        userImage: "userImage",
+        userName: "userName"),
+    StoryModel(
+        story:
+            "https://www.mynrma.com.au/-/media/2021-best-cars-ford-mustang-hero-desktop.jpg?h=501&iar=0&w=1140&hash=4DAA34C7D70E8A1501F91AE808C0B351",
+        uid: "100",
+        userImage:
+            "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        userName: "Supun Perera"),
+    StoryModel(
+        story:
+            "https://carnewschina.com/wp-content/uploads/2023/12/001VUOSZgy1hl9lprwxvwj61401hcnpe02.jpg",
+        uid: "200",
+        userImage:
+            "https://images.unsplash.com/photo-1457449940276-e8deed18bfff?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+        userName: "John Doe"),
+    StoryModel(
+        story:
+            "https://akm-img-a-in.tosshub.com/businesstoday/images/story/202312/658e5b5ebba32-xiaomi-su7-ev-to-take-on-tesla-293837105-16x9.jpg?size=948:533",
+        uid: "300",
+        userImage:
+            "https://e1.pxfuel.com/desktop-wallpaper/903/679/desktop-wallpaper-97-aesthetic-best-profile-pic-for-instagram-for-boy-instagram-dp-boys.jpg",
+        userName: "Jane Smith"),
+    StoryModel(
+        story:
+            "https://d2hucwwplm5rxi.cloudfront.net/wp-content/uploads/2021/12/16101521/Popular-Used-American-Cars-Cover-16-12-420x230.jpg",
+        uid: "400",
+        userImage:
+            "https://pxbar.com/wp-content/uploads/2023/09/instagram-profile-picture.jpg",
+        userName: "Michael Johnson"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -49,8 +83,7 @@ class StoriesListView extends StatelessWidget {
                             width: 30,
                             height: 30,
                             decoration: const BoxDecoration(
-                                color: Colors.blue,
-                                shape: BoxShape.circle),
+                                color: Colors.blue, shape: BoxShape.circle),
                             child: const Icon(
                               Icons.add,
                               color: Colors.white,
@@ -77,8 +110,7 @@ class StoriesListView extends StatelessWidget {
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(15),
                               image: DecorationImage(
-                                  image:
-                                      NetworkImage(stories[index].story),
+                                  image: NetworkImage(stories[index].story),
                                   fit: BoxFit.cover,
                                   colorFilter: ColorFilter.mode(
                                       Colors.black.withOpacity(0.2),
@@ -92,13 +124,13 @@ class StoriesListView extends StatelessWidget {
                             height: 35,
                             decoration: BoxDecoration(
                                 shape: BoxShape.circle,
-                                border: Border.all(
-                                    color: Colors.blue, width: 2)),
+                                border:
+                                    Border.all(color: Colors.blue, width: 2)),
                             child: Padding(
                               padding: const EdgeInsets.all(2.0),
                               child: CircleAvatar(
-                                backgroundImage: NetworkImage(
-                                    stories[index].userImage),
+                                backgroundImage:
+                                    NetworkImage(stories[index].userImage),
                               ),
                             ),
                           ),
@@ -106,8 +138,7 @@ class StoriesListView extends StatelessWidget {
                         Align(
                           alignment: Alignment.bottomLeft,
                           child: Padding(
-                            padding:
-                                const EdgeInsets.only(left: 5, bottom: 5),
+                            padding: const EdgeInsets.only(left: 5, bottom: 5),
                             child: Text(
                               stories[index].userName,
                               style: const TextStyle(
